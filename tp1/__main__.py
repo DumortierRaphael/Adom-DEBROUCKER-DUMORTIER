06b2f10b342900e60e5a508824ce895d26a53b33
debroucker <debroucker.tommy1gmail.com>
2022-01-04 14:30:46 +0100

    q2.3 finito

--- tp1/__main__.py
+++ tp1/__main__.py
@@ -2,4 +2,9 @@ import library
 
 if __name__ == '__main__':
     path = library.get_path()
-    matrix = library.matrix(path)
+    dim, matrix = library.matrix_and_dim(path)
+    n = library.get_nb()
+    all_total_cost = library.many_total_coast(n, matrix, dim)
+    print(f'all (with {n} repeats) :', all_total_cost)
+    print('max :', max(all_total_cost))
+    print('min :', min(all_total_cost))
--- tp1/library.py
+++ tp1/library.py
@@ -1,8 +1,20 @@
-def get_path() :
-    letter = str(input('Letter ? (A - F) : '))
-    return f'tp1/file/random{letter.upper()}100.tsp'
+import sys
+import random
 
-def matrix(path) : 
+def get_path() :
+    path = 'tp1/file/randomA100.tsp'
+    if (len(sys.argv) > 1 and sys.argv[1] == 'o'):
+        letter = str(input('Letter ? (A - F) : '))
+        path = f'tp1/file/random{letter.upper()}100.tsp'
+    return path
+
+def get_nb():
+    n = 10
+    if (len(sys.argv) > 2):
+        n = int(sys.argv[2])
+    return n
+
+def matrix_and_dim(path) : 
     with open(path) as file:
         data = file.readlines()
     len_data = len(data)
@@ -12,11 +24,37 @@ def matrix(path) :
     dim = int(data[nb_lines].split('DIMENSION :')[1].split('\n')[0])
     nb_lines += 4
     while nb_lines < len_data:
-        temp = [0 for i in range(nb_iteration)]
-        for i in range(dim-nb_iteration):
+        temp = [0 for _ in range(nb_iteration)]
+        for _ in range(dim-nb_iteration):
             line = data[nb_lines]
             temp.append(int(line))
             nb_lines += 1
         nb_iteration += 1
         matrix.append(temp)
-    return matrix
+    return dim, matrix
+
+def cost(matrix, i, j) :
+    return matrix[i][j]
+
+def total_cost(matrix, points) :
+    len_list = len(points)
+    total_cost = 0
+    for i in range(len_list-1) :
+        point_i = points[i]
+        point_j = points[i+1]
+        if point_i < point_j:
+            temp_cost = cost(matrix, point_i, point_j)
+        elif point_i > point_j:
+            temp_cost = cost(matrix, point_j, point_i)
+        else :
+            temp_cost = 0
+        total_cost += temp_cost
+    return total_cost
+
+def many_total_coast(n, matrix, dim) :
+    all_total_cost = []
+    points = [i for i in range(dim)]
+    for _ in range(n) :
+        random.shuffle(points)
+        all_total_cost.append(total_cost(matrix, points))
+    return all_total_cost
